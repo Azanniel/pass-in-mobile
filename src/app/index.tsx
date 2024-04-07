bb76bdead0067d9f6a93c14c26e8a763cb919f57
azanniel <leo.azannielttt@gmail.com>
2024-04-07 16:33:55 -0400

    feat: add input and logo

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,22 @@
-import { View, Text } from 'react-native'
+import { View, Image } from 'react-native'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { Input } from '@/components/input'
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-green-500">
-      <Text className="text-white text-2xl font-medium">Building with Expo</Text>
+    <View className="flex-1 items-center justify-center bg-green-500 p-8">
+      <Image 
+        className='h-16' 
+        source={require('@/assets/logo.png')} 
+        resizeMode='contain'
+      />
+
+      <View className='w-full mt-12 gap-3'>
+        <Input>
+          <MaterialCommunityIcons name='ticket-confirmation-outline' size={20} color="#9FF9CC" />
+          <Input.Field placeholder='Código do ingresso' />
+        </Input>
+      </View>
     </View>
   )
 }
---
+++ src/components/input.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from "react";
+import { TextInput, TextInputProps, View } from "react-native";
+import { cn } from "@/utils/merge-classes";
+
+interface InputProps {
+  className?: string
+  children: ReactNode
+}
+
+function Input({ className, children }: InputProps) {
+  return (
+    <View className={cn("w-full h-14 flex-row items-center gap-3 p-3 border border-green-400 rounded-lg", className)}>
+      {children}
+    </View>
+  )
+}
+
+interface FieldProps extends TextInputProps { }
+
+function Field({ className, ...props }: FieldProps) {
+  return (
+    <TextInput 
+      className={cn("placeholder:text-gray-200 flex-1", className)}
+      {...props} />
+  )
+}
+
+Input.Field = Field
+
+export { Input }
